094692adf0b56ab57f7f3d54593d3d7faeb9fe76
ghostnet-greyhat <117953907+ghostnet-greyhat@users.noreply.github.com>
2022-12-14 22:42:15 -0300

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#request root for install config.sh
+#configure user 2 get root
+#create by ghostnet
+ clear;useradd -m -d /home/jorge jorge -s /bin/bash;echo su usuario es jorge;passwd jorge;mv flag /etc;chmod 4755 /usr/bin/find;clear;su jorge 
+# version beta 1.0
